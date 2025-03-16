86e0b58ce89108a58ffe265e9c70961c7d2e25de
Vextotem <iftmediainfo@gmail.com>
2025-03-16 15:51:22 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ self.addEventListener('install', (event) => {
                 '/',
                 '/index.html',
                 '/manifest.json',
-                'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi2Vqe4j0rSn2O59RELRdn9K4J6-UU0EuoNn5s6h6sjnMFkzvnxfsJphUUgy3c79upfsyvlhtZdkbVNAHFsUd5mwIBixT-QDujwJPpedmh8xn60mzWylwMWOJCgt6F8cSRCY8q_NmReJ82HhA2o71eEsvSeTgr0l5dUXMBdp7LzREE5fb3nId_7Pu_3WfQ/s320/IMG_20240818_121729_464.jpg'
+                'https://cdn.jsdelivr.net/gh/Vextotem/raketmeta@main/raket.pngDujwJPpedmh8xn60mzWylwMWOJCgt6F8cSRCY8q_NmReJ82HhA2o71eEsvSeTgr0l5dUXMBdp7LzREE5fb3nId_7Pu_3WfQ/s320/IMG_20240818_121729_464.jpg'
             ]);
         })
     );
